e430894cfc87c4da5ae18383dae3bc0e2860e53d
WanDev <71076451+alwanfa@users.noreply.github.com>
2022-11-10 16:41:40 +0700

    edit something

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -4,40 +4,20 @@ use Illuminate\Support\ServiceProvider;
 class ContactServiceProvider extends ServiceProvider{
     public function boot(){
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        // $this->loadViewsFrom(__DIR__.'/views', 'contact');
+        $this->loadViewsFrom(__DIR__.'/views', 'contact');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         
         $this->mergeConfigFrom(
-            __DIR__.'/config/contact.php', 
-            'contact'
-        );
+            __DIR__.'/config/contact.php', 'contact');
         $this->publishes([
-            __DIR__.'/config/contact.php' => config_path('contact.php')
-        ]);
-        $this->publishes([
+            __DIR__.'/config/contact.php' => config_path('contact.php'),
             __DIR__.'/public/contact.css' => public_path('vendor/contact/contact.css'),
-        ], 'public');
-
-        $this->publishes([
-            __DIR__.'/views/contact' => resource_path('views/contact/contact'),
-        ], 'public');
-
-        $this->publishes([
-            __DIR__.'/views/contact.blade.php' => resource_path('views/contact/contactView.blade.php'),
-        ], 'public');
-
-        $this->publishes([
-            __DIR__.'/database/migrations/2022_10_27_094034_create_contacts_table.php' => app_path('/database/migrations/2022_10_27_094034_create_contacts_table.php'),
-        ], 'public');
-        $this->publishes([
+            __DIR__.'/views' => resource_path('views/contact'),
+            __DIR__.'/database/migrations/2022_10_27_094034_create_contacts_table.php' => database_path('/migrations/2022_10_27_094034_create_contacts_table.php'),
             __DIR__.'/Models/contact.php' => app_path('/Models/Contact.php'),
-        ], 'public');
-        $this->publishes([
             __DIR__.'/Http/Controllers/ContactController.php' => app_path('/Http/Controllers/ContactController.php'),
-        ], 'public');
-        $this->publishes([
-            __DIR__.'/Mail' => app_path('/Mail'),
-        ], 'public');     
+            __DIR__.'/Mail' => app_path('/Mail')
+        ]); 
     }
     public function register()
     {
--- src/Http/Controllers/ContactController.php
+++ src/Http/Controllers/ContactController.php
@@ -12,7 +12,7 @@ class ContactController extends Controller
 {
      public function display()
     {
-        return view('contact/contactView');
+        return view('contact::contact');
     }
     public function store(Request $req)
     {
